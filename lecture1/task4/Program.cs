1cc9946ba264cd3e1a931e2568bc74e1361a3495
Alexandr-xxx <alexneoas@mail.ru>
2024-10-21 23:27:45 +0300

    add lecture1:task4

---
+++ lecture1/task4/Program.cs
@@ -0,0 +1,43 @@
+﻿int a = 555;
+int b = -677;
+int c = 817;
+int d = 976;
+int e = -95;
+
+int max = a;
+int min = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+if (b < min)
+{
+    min = b;
+}
+if (c < min)
+{
+    min = c;
+}
+if (d < min)
+{
+    min = d;
+}
+if (e < min)
+{
+    min = e;
+}
+
+Console.WriteLine($"Из чесел {a}, {b}, {c}, {d}, {e}, наибольшее число {max}, наименьшее число {min}\n");
