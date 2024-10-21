1ce36d79462b6c948c04d3e1e836e0e907985c9a
Alexandr-xxx <alexneoas@mail.ru>
2024-10-21 20:54:34 +0300

    Home work (task1), lecture1

---
+++ lecture1/home_work/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 10;
+long b = 150;
+double c = 2.5;
+double d = 7.5;
+
+long product = a * b;
+double sum = c + d;
+
+double quotient = product / sum;
+
+Console.WriteLine($"Даны переменные a = {a}; b = {b}; c = {c}; d = {d}; \n");
+//
+Console.WriteLine("Результат примера:");
+Console.WriteLine($"(a * b) / (c + d) = {quotient} \n");
