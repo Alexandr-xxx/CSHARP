8722f26a9f9dd58b9a1ffc5e557762fe7d2033ea
Alexandr-xxx <alexneoas@mail.ru>
2024-10-21 22:40:27 +0300

    add lecture1:task3

---
+++ lecture1/task3/Program.cs
@@ -0,0 +1,20 @@
+﻿int a = 55;
+int b = -7;
+
+if (a > b)
+{
+    Console.WriteLine($"Из чесел {a} и {b} наибольшее число {a}");
+}
+else
+{
+    Console.WriteLine($"Из чесел {a} и {b} наибольшее число {b}");
+}
+
+if (a < b)
+{
+    Console.WriteLine($"Наименьшее число {a}");
+}
+else
+{
+    Console.WriteLine($"Наименьшее число {b}");
+}
