5d6e44ac2d3c700fb629aaa580902bd377969c1a
Alexandr-xxx <alexneoas@mail.ru>
2024-10-21 22:50:36 +0300

    made edits lecture1:task3

--- lecture1/task3/Program.cs
+++ lecture1/task3/Program.cs
@@ -3,18 +3,9 @@ int b = -7;
 
 if (a > b)
 {
-    Console.WriteLine($"Из чесел {a} и {b} наибольшее число {a}");
+    Console.WriteLine($"Из чесел {a} и {b} наибольшее число {a}, наименьшее число {b}");
 }
 else
 {
-    Console.WriteLine($"Из чесел {a} и {b} наибольшее число {b}");
-}
-
-if (a < b)
-{
-    Console.WriteLine($"Наименьшее число {a}");
-}
-else
-{
-    Console.WriteLine($"Наименьшее число {b}");
+    Console.WriteLine($"Из чесел {a} и {b} наибольшее число {b}, наименьшее число {a}");
 }
