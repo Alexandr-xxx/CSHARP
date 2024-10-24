d0c60e3fddb1893a104532115126d4f4e33e3295
Alexandr-xxx <alexneoas@mail.ru>
2024-10-24 15:53:01 +0300

    add seminar1\home_work_1: task4

---
+++ seminar1/home_work_1/task4/Program.cs
@@ -0,0 +1,28 @@
+﻿// Написать программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N. 
+// Пример:
+// 5 => 2, 4
+// 8 => 2, 4, 6, 8 
+
+
+Console.Write("Введите натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = 2;
+
+if (num > 0)
+{
+    while (count <= num)
+    {
+        if (count % 2 == 0)
+        {
+            Console.Write(count + " ");
+            count = count + 2;
+        }
+
+    }
+}
+else
+{
+   Console.WriteLine("Ошибка ввода");
+}
+
