066b9d286d8ca7c2b5fc5c3679e4d6daff1980fa
Alexandr-xxx <alexneoas@mail.ru>
2024-10-26 14:04:24 +0300

    add lecture2: task2

---
+++ lecture2/task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Создать массив и заполнить его целыми
+// числами от 1 до N (N – произвольное
+// натуральное число). Вывести элементы
+// массива на экран.
+
+Console.Write("Введите натуральное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = new int[n];
+int i = 0; // i - это счётчик
+
+while (i < arr.Length)
+{
+    arr[i] = i + 1;
+    Console.Write($"{arr[i]} "); // Console.Write(arr[i] + " ");
+    i++; // i = i + 1;
+}
+
