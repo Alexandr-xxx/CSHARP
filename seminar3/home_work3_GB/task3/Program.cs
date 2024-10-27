4ebec10002d5d21b1852365edd600438f672eb38
Alexandr-xxx <alexneoas@mail.ru>
2024-10-27 22:30:29 +0300

    add seminar3\home_work3_GB: task3

---
+++ seminar3/home_work3_GB/task3/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задание 3: Поиск среднего значения массива
+// Описание: Задайте массив из 10 целых чисел. Найдите среднее значение элементов
+// массива.
+
+int[] array = new int[10] { 6, 1, 9, 2, 5, 3, 8, 7, 10, 4 };
+
+double sum = 0;
+double result = 0;
+int count = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    sum = sum + array[i];
+    count++;
+}
+
+result = sum / count;
+
+Console.Write($"Cреднее значение элементов массива равно: {result}");
