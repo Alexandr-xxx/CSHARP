693d1bee7e7d7bdfe2aaa8e7c082bafe38e38d70
Alexandr-xxx <alexneoas@mail.ru>
2024-10-27 16:51:46 +0300

    add seminar3: task1

---
+++ seminar3/task1/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте массив. Напишите программу, которая определяет,
+// присутствует ли заданное число в массиве. Программа
+// должна выдать ответ: Да/Нет.
+// Примеры:
+// [ 1, 3, 4, 19, 3], 8 => Нет
+// [ -4, 3, 4, 1 ], 3 => Да
+
+// int[] array1 = new int[5];
+// int[] array2 = new int[5] { 1, 3, 4, 19, 3 };
+// int[] array3 = new int[] { 1, 3, 4, 19, 3 };
+
+int[] array = { 1, 3, 4, 19, 7 };
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool isExitNum = false;
+
+for (int i = 0; i < array.Length; i++)
+{
+     if (array[i] == num)
+    {
+         isExitNum = true;
+         break;
+    }
+}
+
+
+string result = isExitNum ? "Дв, введенное число присутствует в массиве" : "Нет, введенное число не присутствует в массиве";
+Console.WriteLine(result); // Console.WriteLine(isExitNum ? "Дв, введенное число присутствует в массиве" : "Нет, введенное число не присутствует в массиве");
+
