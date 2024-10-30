e63689f1a930dcdddec5fd80f8b1133989698c03
Alexandr-xxx <alexneoas@mail.ru>
2024-10-30 14:02:36 +0300

    add lecture3: task1

---
+++ lecture3/task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Вычислить значение формулы (a*b) / ( c+ d),
+// где a, b, c, d - некоторые целые числа. Решение
+// оформить в виде функции.
+
+
+double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denominator = c + d;
+    double result = numenator / denominator;
+    return result;
+}
+
+double result = CalculateFormula(6, 5, 7, 3);
+Console.WriteLine(result);
