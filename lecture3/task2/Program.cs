a936a98e6de891bfe8845143ca7625b12d75ec2d
Alexandr-xxx <alexneoas@mail.ru>
2024-10-30 15:28:40 +0300

    add lecture3: task2

---
+++ lecture3/task2/Program.cs
@@ -0,0 +1,13 @@
+﻿void PrintSquare(int limit)
+{
+    int i = 1;
+    while (i <= limit)
+    {
+        Console.Write($"{i*i} ");
+        i++; // i = i + 1;
+    }
+}
+
+PrintSquare(5);
+PrintSquare(10);
+PrintSquare(15);
