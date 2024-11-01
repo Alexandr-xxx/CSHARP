eef2d9f547ae0cf1f4f1ab5bd8ee7799d2bdbb1f
Alexandr-xxx <alexneoas@mail.ru>
2024-11-02 02:03:52 +0300

    edit  seminar4\home_work_4: task3

--- seminar4/home_work_4/task3/Program.cs
+++ seminar4/home_work_4/task3/Program.cs
@@ -4,8 +4,18 @@
 // Пример:
 // [1 3 5 6 7 8] => [8 7 6 5 3 1]
 
+int[] CreatArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
 
-int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+
+    return array;
+}
 
 void PrintArray(int[] array)
 {
@@ -26,22 +36,18 @@ void PrintArray(int[] array)
     Console.Write("]");
 }
 
-PrintArray(array);
-
-void InvertArray(int[] array)
+void InvertedtArray(int[] array)
 {
-    // int temp;
+    int temp;
     for (int i = 0; i < array.Length / 2; i++)
     {
-        int temp = array[i];
+        temp = array[i];
         array[i] = array[array.Length - 1 - i];
         array[array.Length - 1 - i] = temp;
     }
 }
 
-InvertArray(array);
-
-void PrintInvertArray(int[] array)
+void PrintInvertedArray(int[] array)
 {
     Console.Write("[");
     
@@ -60,5 +66,11 @@ void PrintInvertArray(int[] array)
     Console.Write("]");
 }
 
+Console.Write("Введите размер массива: ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreatArrayRndInt(sizeArray, 1, 11);
+PrintArray(arr);
+InvertedtArray(arr);
 Console.Write($" Перевернутый массив => ");
-PrintInvertArray(array);
+PrintInvertedArray(arr);
