7210d91857f0056ca2f428fbf3889d06f208c22d
Alexandr-xxx <alexneoas@mail.ru>
2024-11-01 23:16:28 +0300

    add seminar4\home_work_4: task 2

---
+++ seminar4/home_work_4/task2/Program.cs
@@ -0,0 +1,59 @@
+﻿// Задайте массив заполненный случайными
+// трёхзначными числами. Напишите программу,
+// которая покажет количество чётных чисел в
+// массиве.
+// Пример:
+// [344 452 341 125] => 2
+
+int[] CreatArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]} ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}");
+        }
+    }
+
+    Console.Write("]");
+}
+
+int CountOfEvenNumbers(int[] array)
+{
+    int count = 0;
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int[] arr = CreatArrayRndInt(10, 100, 1000);
+PrintArray(arr);
+
+int countEven = CountOfEvenNumbers(arr);
+Console.Write($" => {countEven}");
