085ed74ea65129b54b7e1af0bc2aca578a5faa5b
Alexandr-xxx <alexneoas@mail.ru>
2024-11-06 19:03:14 +0300

    add seminar5\home_work_GB_5: task4

---
+++ seminar5/home_work_GB_5/task4/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задание 4: Замена отрицательных элементов в двумерном массиве
+// Описание: Задайте двумерный массив. Замените все отрицательные числа на их
+// абсолютные значения.
+// Пример входных данных:
+// 1 -2 3
+// -4 5 -6
+// 7 -8 9
+// Пример результата:
+// 1 2 3
+// 4 5 6
+// 7 8 9
+
+int[,] CreateMatrixIntRnd(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max);
+        }
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ReplacingNegativeValuesMatrixWithAbsolute(int[,] matrix)
+{    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(matrix[i,j] < 0)
+            {
+                matrix[i,j] *= -1; // matrix[i,j] = matrix[i,j] * -1;
+            }
+            Console.Write($"{matrix[i, j],4} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array2d = CreateMatrixIntRnd(3, 5, -10, 10);
+ShowMatrix(array2d);
+
+Console.WriteLine();
+
+ReplacingNegativeValuesMatrixWithAbsolute(array2d);
