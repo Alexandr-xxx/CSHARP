999bd83282b5d33351c9478eb19409844acee8f3
Alexandr-xxx <alexneoas@mail.ru>
2024-11-12 18:44:08 +0300

    add  lecture5: home_work_5

---
+++ lecture5/home_work_5/Program.cs
@@ -0,0 +1,14 @@
+﻿int Fact(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        Console.WriteLine($"Stop requrson: n={n}");
+        return 1;
+    }
+    Console.WriteLine(n);
+    int result = n * Fact(n - 1);
+    Console.WriteLine($"Возврат: n={n}, fact={result / n}");
+    return result;
+}
+
+Console.WriteLine(Fact(5));
