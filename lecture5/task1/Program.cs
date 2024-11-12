86cd0022364c935aa6c21db370fa13346376077b
Alexandr-xxx <alexneoas@mail.ru>
2024-11-12 17:50:09 +0300

    add lecture5: task1

---
+++ lecture5/task1/Program.cs
@@ -0,0 +1,14 @@
+﻿int Fact(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        Console.WriteLine($"Stop requrson: n={n}");
+        return 1;
+    }
+    Console.WriteLine(n);    
+    return n * Fact(n - 1);
+}
+
+// int result = Fact(5);
+// Console.WriteLine(result);
+Console.WriteLine(Fact(5));
