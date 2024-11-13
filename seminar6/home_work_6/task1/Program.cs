c375dcebb8c37ecc16d37ff960eefe21d7936bf3
Alexandr-xxx <alexneoas@mail.ru>
2024-11-13 20:10:27 +0300

    add seminar6\home_work_6: task1

---
+++ seminar6/home_work_6/task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 1: Задайте двумерный массив символов (тип char
+// [,]). Создать строку из символов этого массива.
+// Пример:
+// a b c => “abcdef”
+// d e f
+
+string CreateStringOfCharMatrix(char[,] matrix)
+{
+    string result = string.Empty;
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            result += matrix[i, j];
+        }
+    }
+    return result;
+}
+
+char[,] matrixChar = new char[2, 3]
+{
+{'a', 'b', 'c'},
+{'d', 'e', 'f'},
+};
+
+string res = CreateStringOfCharMatrix(matrixChar);
+Console.WriteLine(res);
