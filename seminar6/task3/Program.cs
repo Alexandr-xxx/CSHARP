0007872281e9e1fb7ca5ed0ffd42df35b6649959
Alexandr-xxx <alexneoas@mail.ru>
2024-11-13 17:40:34 +0300

    add seminar6: task3

---
+++ seminar6/task3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Считать строку с консоли, состоящую из латинских
+// букв в нижнем регистре. Выяснить, сколько среди
+// введённых букв гласных.
+// Пример:
+// “hello” => 2
+// “world” => 1
+
+
+int CountOfVowelLetters(string str)
+{
+    string vowels = "aeiouy";
+    int count = 0;
+
+    for (int i = 0; i < str.Length; i++)
+    {
+        for (int j = 0; j < vowels.Length; j++)
+        {
+            if (vowels[j] == str[i])
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+
+Console.Write("Введите строку из латинских букв в нижнем регистре: ");
+string str = Console.ReadLine();
+Console.WriteLine($"Количество гласных равно: {CountOfVowelLetters(str)}");
