e0422477ac3ff211263c4722f65965ebbf41674c
Alexandr-xxx <alexneoas@mail.ru>
2024-11-14 13:23:05 +0300

    add seminar6\home_work_6: task3

---
+++ seminar6/home_work_6/task3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните,
+// является ли она палиндромом..
+// Пример:
+// “aBcD1ef!-” => Нет
+// “шалаш” => Да
+// “55655” => Да
+
+void ShowAPalindromeOrNotAPalindrome(string[] args)
+{
+    string input = "а роза упала на лапу азора";
+    // string input = "aBcD1ef!-";
+    // string input = "шалаш";
+    // string input = "55655";
+    bool isPalindrome = StringСonversionRemovingNonAlphanumericCharTolower(input);
+    Console.WriteLine(isPalindrome ? "Да" : "Нет");
+}
+
+bool StringСonversionRemovingNonAlphanumericCharTolower(string str)
+{
+    string normalized = new
+    string(str.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+    return normalized.SequenceEqual(normalized.Reverse());
+}
+
+ShowAPalindromeOrNotAPalindrome(args);
