384a5935d950a0c213c3420cb124d4acff248069
Alexandr-xxx <alexneoas@mail.ru>
2024-11-18 11:47:20 +0300

    add seminar7/home_work_7: task3

---
+++ seminar7/home_work_7/task3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+// Пример:
+// [1, 2, 5, 0, 10, 34] => 34 10 0 5 2 1
+
+
+void ReverseArray(int[] arr, int index)
+{
+    if (index < 0)
+    {
+        return;
+    }
+
+    if (index > 0)
+    {
+        Console.Write($"{arr[index]}, ");
+        ReverseArray(arr, index - 1);
+    }
+
+    else
+    {
+        Console.Write($"{arr[index]}");
+    }
+}
+
+// int[] array = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+int[] array = new int[10] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+Console.WriteLine("Массив в обратном порядке: ");
+ReverseArray(array, array.Length - 1);
